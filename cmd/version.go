9b67f77ac88a6bbcda53902b140a71dbc7720b8f
mu853 <mu.youichi853@gmail.com>
2022-07-16 20:22:14 +0900

    add subcommand to show nsx-t version

--- cmd/version.go
+++ cmd/version.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/spf13/cobra"
 )
@@ -13,6 +14,33 @@ func NewCmdVersion() *cobra.Command {
 		Short: "show version of nsxctl",
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Printf("nsxctl version: %s, revision: %s\n", version, revision)
+		},
+	}
+
+	return versionCmd
+}
+
+// show NSX-T version
+func NewCmdShowVersion() *cobra.Command {
+	aliases := []string{"v"}
+	versionCmd := &cobra.Command{
+		Use:     "version",
+		Aliases: aliases,
+		Short:   "show version of NSX-T",
+		PreRunE: func(c *cobra.Command, args []string) error {
+			site, err := conf.NsxT.GetCurrentSite()
+			if err != nil {
+				log.Fatal(err)
+			}
+			nsxtclient.Login(site.GetCredential())
+			return nil
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println(nsxtclient.GetVersion())
+		},
+		PostRunE: func(c *cobra.Command, args []string) error {
+			nsxtclient.Logout()
+			return nil
 		},
 	}
 
