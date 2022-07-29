7b8bfb01d333a4263ebcf5802761bc25f1f73022
mu853 <mu.youichi853@gmail.com>
2022-07-29 23:37:05 +0900

    add show dfw-policies and dfw-rules

--- structs/nsxalb.go
+++ structs/nsxalb.go
@@ -90,7 +90,7 @@ type VSResult struct {
 
 type VirtualServiceInventory struct {
 	Config  VirtualService `json:"config"`
-	Health  map[string]int `json:"heal`
+	Health  map[string]int `json:"heal"`
 	Runtime VSRuntime      `json:"runtime"`
 }
 
